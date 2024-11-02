df94665ac55146cc5f2af559ba8c01215a1921c6
ruspian <ruspianntb@gmail.com>
2024-11-02 18:14:26 +0800

    memanggil method dari modul lain

--- src/cpu/cpu.service.ts
+++ src/cpu/cpu.service.ts
@@ -4,4 +4,10 @@ import { PowerService } from 'src/power/power.service';
 @Injectable()
 export class CpuService {
   constructor(private powerService: PowerService) {}
+
+  compute(a: number, b: number) {
+    console.log('CPU membutuhkan 10 watt untuk menghitung');
+    this.powerService.suplyPower(10, 'CPU');
+    return a + b;
+  }
 }
--- src/disk/disk.service.ts
+++ src/disk/disk.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { PowerService } from 'src/power/power.service';
 
 @Injectable()
-export class DiskService {}
+export class DiskService {
+  constructor(private powerService: PowerService) {}
+
+  getData() {
+    console.log('Disk membutuhkan 5 watt untuk menampilkan data');
+    this.powerService.suplyPower(5, 'Disk');
+    return 'Data yang diambil dari disk';
+  }
+}
